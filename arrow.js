7695ed8e15e3f167a6bd5c8b61876e64163e86a4
Vance Posey <poseyv12@gmail.com>
2023-03-11 16:34:46 -0600

    addes js file

---
+++ arrow.js
@@ -0,0 +1,30 @@
+let circle = document.querySelector(".circle")
+let moveBy = 20
+
+var rect = circle.getBoundingClientRect();
+
+window.addEventListener("load", () => {
+    circle.style.position = "absolute";
+    circle.style.left = 0;
+    circle.style.top = 0;
+});
+
+window.addEventListener("keydown",  (e) =>{
+console.log(rect.top, rect.right, rect.bottom, rect.left);
+    
+    switch(e.key){
+        case 'ArrowLeft':
+            circle.style.left = parseInt(circle.style.left) - moveBy + 'px';
+            break;
+        case 'ArrowRight':
+            circle.style.left = parseInt(circle.style.left) + moveBy + 'px';
+            break;
+        case 'ArrowUp':
+            circle.style.top = parseInt(circle.style.top) - moveBy + 'px';
+            break;
+        case 'ArrowDown':
+            circle.style.top = parseInt(circle.style.top) + moveBy + 'px';
+            break;
+    }
+}
+)
